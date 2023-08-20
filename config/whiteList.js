7e02860dd055a7c1718924868515146e09db5e6a
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-08-21 02:53:58 +0530

    white list updated

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,11 +1,7 @@
 // cors - cross origin resource sharing
 const whitelist = [
-  "http://localhost:3500",
-  "http://localhost:3000",
+  "https://quickpens.netlify.app",
   "http://localhost:5173",
-  "https://jocular-flan-eb5c54.netlify.app",
-  "http://localhost:5173",
-  "https://main--jocular-flan-eb5c54.netlify.app"
 ];
 
 module.exports = whitelist
